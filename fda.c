d183f74f1dd827184a902b4375549b041ed3a6b8
Dylan T Schillaci <dtschill@timberlake.cse.buffalo.edu>
2020-05-08 19:01:02 -0400

    added main function, with counter function

--- fda.c
+++ fda.c
@@ -2,3 +2,39 @@
 #include <string.h>
 #include <stdio.h>
 #include "fda.h"
+
+int main(int argc, char* argv[]) {
+    char buffer[1024] ;
+    char *record,*line;
+    int i=0,j=0;
+    int arr[10][200];
+    int counter=0;
+
+    FILE *fstream = fopen("2018Recalls.xml","r");
+    if(fstream == NULL) {
+       printf("\n file opening failed ");
+       return -1 ;
+    }
+    while((line=fgets(buffer,sizeof(buffer),fstream))!=NULL) {
+        char *token = strtok(buffer,",");
+
+        while( token != NULL ) {
+            counter++;
+            printf( " %s\n",token);
+            token = strtok(NULL,",");  
+        }
+
+    }
+    printf(*argv[2]);
+    if (*argv[2]=='-counter')
+    {
+        printf("%d /n", counter);
+
+    }
+
+    return 0;
+}
+
+
+
+
