a2d581a04213ddffdfde25ffe8bca0af12e2da31
Hussein Atwa Barakat <hatwabar@timberlake.cse.buffalo.edu>
2020-05-08 19:48:06 -0400

    count func written

--- fda.c
+++ fda.c
@@ -3,5 +3,29 @@
 #include <stdio.h>
 #include "fda.h"
 
+int count (char * filename){
+  char buffer[1024] ;
+  char *record,*line;
+  int i=0,j=0;
+  int arr[10][200];
+  int counter=0;
+
+  FILE *fstream = fopen("2018Recalls.xml","r");
+  if(fstream == NULL) {
+    printf("\n file opening failed ");
+    return -1 ;
+  }
+  while((line=fgets(buffer,sizeof(buffer),fstream))!=NULL) {
+    char *token = strtok(buffer,"<Brand>");
+
+    while( token != NULL ) {
+      counter++;
+      printf( " %s\n",token);
+      token = strtok(NULL,",");
+    }
+
+  }
+  return counter;
+}
 
 
